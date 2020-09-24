b83241770f3699b1b94c135ad0de94b5180aa17e
jonathandowdell <8318Pro2711>
2020-09-23 20:36:58 -0500

    User Base Initial Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserService } from './user/user.service';
-import { UserController } from './controller/user/user.controller';
-import { UserService } from './service/user/user.service';
-import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
