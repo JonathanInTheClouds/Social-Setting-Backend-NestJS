13997524083bc7b2bd2f4bb4f49f0ff54a6c7f7c
jonathandowdell <8318Pro2711>
2020-09-23 20:31:15 -0500

    First Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserService } from './user/user.service';
+import { UserController } from './controller/user/user.controller';
+import { UserService } from './service/user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
